700aaa5d89bafabf87876ea33aeb4b8083c0b41a
Phil Jirsa <phil.jirsa@microsoft.com>
2022-03-22 08:15:18 -0500

    Add img and workflow timeout

---
+++ src/Models/EncounterStart/EncounterStartContext.cs
@@ -0,0 +1,9 @@
+﻿using CDSService.Models.PatientView;
+
+namespace Models.EncounterStart
+{
+    public class EncounterStartContext : PatientViewContext
+    {
+        public string EncounterId { get; set; }
+    }
+}
